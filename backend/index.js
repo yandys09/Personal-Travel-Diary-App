168967a68ca4e052c567135544b6de0878b32b6d
yandys09 <yandys03@naver.com>
2025-05-18 20:56:57 +0900

    Connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 import express from "express";
+import colors from "colors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database is connected!!.".bgCyan);
+  })
+  .catch((err) => {
+    console.log("err : ".bgRed, err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!.");
+  console.log("Server is running on port 3000!.".bgMagenta);
 });
-
-
