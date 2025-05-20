50550ed63312c5649af2763f89b4270e56e2929a
yandys09 <yandys03@naver.com>
2025-05-20 20:04:12 +0900

    create signup api route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import colors from "colors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -16,6 +17,11 @@ mongoose
 
 const app = express();
 
+//for allowing json object in req body
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!.".bgMagenta);
 });
+
+app.use("/api/auth", authRoutes);
