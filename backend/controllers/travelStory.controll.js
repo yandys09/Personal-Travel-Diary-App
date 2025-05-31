6852e188cae94fa5752769746687b30f6b1a37a0
yandys09 <yandys03@naver.com>
2025-05-31 21:51:00 +0900

    delete image api route

--- backend/controllers/travelStory.controll.js
+++ backend/controllers/travelStory.controll.js
@@ -1,5 +1,8 @@
+import { fileURLToPath } from "url";
 import TravelStory from "../models/travelStory.model.js";
 import { errorHandler } from "../utils/error.js";
+import path from "path";
+import fs from "fs"
 
 export const addTravelStory = async (req, res, next) => {
   const { title, story, visitedLocation, imageUrl, visitedDate } = req.body;
@@ -41,22 +44,56 @@ export const getAllTravelStory = async (req, res, nect) => {
       isFavorite: -1,
     });
 
-    res.status(200).json({ stories: travelStories})
+    res.status(200).json({ stories: travelStories });
   } catch (error) {
     next(error);
   }
 };
 
-export const imageUpload = async(req, res, next) => {
+export const imageUpload = async (req, res, next) => {
   try {
-    if(!req.file){
-      return next(errorHandler(400, "No image uploaded."))
+    if (!req.file) {
+      return next(errorHandler(400, "No image uploaded."));
     }
 
-    const imageUrl = `http://localhost:3000/uploads/${req.file.filename}`
+    const imageUrl = `http://localhost:3000/uploads/${req.file.filename}`;
 
-    res.status(201).json({ imageUrl})
+    res.status(201).json({ imageUrl });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const rootDir = path.join(__dirname, "..")
+
+export const deleteImage = async (req, res, next) => {
+  const { imageUrl } = req.query;
+
+  if (!imageUrl) {
+    return next(errorHandler(400, "ImageURL parameter is required."));
+  }
+  try {
+    // extract the filename from the imageurl
+    const filename = path.basename(imageUrl);
+
+    // Delete the file path
+    const filePath = path.join(rootDir, "uploads", filename);
+
+    console.log(filePath)
+
+    // check if the file exists
+    if(!fs.existsSync(filePath)){
+      return next(errorHandler(400, "Image not found!."))
+    }
+
+    //delete the file
+    await fs.promises.unlink(filePath)
+
+    res.status(200).json({message: "Image deleted successfully!."})
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/travel.route.js
+++ backend/routes/travel.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { verifyToken } from "./../utils/verifyUser.js";
 import {
   addTravelStory,
+  deleteImage,
   getAllTravelStory,
   imageUpload,
 } from "../controllers/travelStory.controll.js";
@@ -9,9 +10,9 @@ import upload from "../multer.js";
 
 const router = express.Router();
 
-router.post("/image-upload", upload.single("image"), imageUpload)
+router.post("/image-upload", upload.single("image"), imageUpload);
+router.delete("/delete-image", deleteImage);
 router.post("/add", verifyToken, addTravelStory);
-
 router.get("/get-all", verifyToken, getAllTravelStory);
 
 export default router;
